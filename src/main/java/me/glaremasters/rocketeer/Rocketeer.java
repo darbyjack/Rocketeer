edb3e68cd3a0081a4f920365b12b99f27110e1f2
darbyjack <admin@glaremasters.me>
2020-10-23 18:43:40 -0500

    Initial Commit

---
+++ src/main/java/me/glaremasters/rocketeer/Rocketeer.java
@@ -0,0 +1,18 @@
+package me.glaremasters.rocketeer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Rocketeer extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+
+}
